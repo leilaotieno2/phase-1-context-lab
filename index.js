6ebb7a915a72236c9b7927f08130dc0c18c13659
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-07 13:05:41 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,105 @@
 /* Your Code Here */
+// employee.js
+
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date,
+    });
+    return this;
+  }
+  
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date,
+    });
+    return this;
+  }
+  
+  function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find((event) => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find((event) => event.date === date);
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    const payRate = this.payPerHour;
+    const wagesEarned = hoursWorked * payRate;
+    return wagesEarned;
+  }
+  
+  module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate
+  };
+  
+  // payroll.js
+  
+  const {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate
+  } = require('./employee');
+  
+  const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+      return e.date
+    })
+  
+    const payable = eligibleDates.reduce(function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0)
+  
+    return payable
+  }
+  
+  function findEmployeeByFirstName(employees, firstNameString) {
+    return employees.find(function (employee) {
+      return employee.firstName === firstNameString
+    })
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce(function (memo, employee) {
+      return memo + allWagesFor.call(employee)
+    }, 0)
+  }
+  
+  module.exports = {
+    allWagesFor,
+    findEmployeeByFirstName,
+    calculatePayroll
+  };
+  
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
